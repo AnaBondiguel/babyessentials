ef522aa877ac2305d032557c921e127503fafc74
AnaBondiguel <michaelyangyang@hotmail.com>
2022-06-21 18:16:21 +1000

    add condition enum

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,11 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+  enum condition: {
+    very_used_condition: 1,
+    good_used_condition: 2,
+    used_once: 3,
+    brand_new: 4
+  }
 end
